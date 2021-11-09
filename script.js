995a986b697e37e074b70597e9636aff763fa7bf
21900884 <90634157+21900884@users.noreply.github.com>
2021-11-09 09:25:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,16 +14,20 @@ function processing() {
         
         var i = jurus;
         var n = parcelas;
-        //var situacao = "";
+        var situacao = "";
         var PV = 0;
         var PMT = 0;
         var ValorMaximo = 0;
+        
+        window.alert("Teste");
         
         PV = (preco - entrada).toFixed(2);
         
         PMT = PV * (((1 + i) ** n * i) / ((1 + i) ** n - 1)).toFixed(2);
         
         ValorMaximo = (30/100) * salario.toFixed(2);
+        
+        const X = 1+1;
         
         if (PMT <= ValorMaximo) {
             situacao = "Aprovado";
@@ -32,7 +36,9 @@ function processing() {
             situacao = "Recusado";
         }
 
-
+        
+        window.alert(X);
+        
         resultado.textContent = `Olá ${nome}! O valor ${PV} foi parcelado em ${parcelas} parcelas com ${jurus}% de jurus, resultando em R$ ${PMT} por parcela. Tendo em vista o seu salario de R$ ${salario} e o seu limite de R$ ${ValorMaximo} para cada parcela, o seu financiamento foi ${situacao}!`;
 
     }else {
